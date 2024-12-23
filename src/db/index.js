e3db25a05b70faed900a93eb1a19ebc8d7bad6c4
Shoyeb45 <shoyebff45@gmail.com>
2024-12-23 10:54:11 +0530

    Succefully conneccted mongo DB using mongoose

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+    try {
+
+        let uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
+        console.log("uri", uri);
+        
+        const connectionInstance = await mongoose.connect(uri);
+        console.log(`----------- MongoDB connected successfully -----------\nDatabase host: ${connectionInstance.connection.host}\n------------------------ Happy -----------------------`);
+        
+    } catch (error) {
+        console.log("MongoDB Connection failed");
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
