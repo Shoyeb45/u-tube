1aa990bcdb418eefde17a3950d2fb4fd4637f459
Shoyeb45 <shoyebff45@gmail.com>
2024-12-25 10:08:39 +0530

    Added route point for registering the user

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 
 // routes import
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.route.js";
 
 // Routes declaration
 app.use("/api/v1/user", userRouter); // using api and v1 is good practice 
